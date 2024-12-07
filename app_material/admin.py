f85cb285b345e86edebfaac80a9cd7e6928d114a
Pinchuk Sergey <Pinboyer@gmail.com>
2024-12-07 20:38:39 +0300

    add material models to admin site

--- app_material/admin.py
+++ app_material/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_material.models import Section, Material, Subscription
+from app_test.models import Test, Question, Answer, TestTry
+
+admin.site.register(Section)
+admin.site.register(Material)
+admin.site.register(Subscription)
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(TestTry)
+
